d9042db6058b5c7c5f6c11d47392492942818300
MishaHnatyshyn <gnatyshynmisha@gmail.com>
2020-03-01 17:00:41 +0200

    base search logic

--- src/store/initialSaga.js
+++ src/store/initialSaga.js
@@ -1,8 +1,10 @@
 import { all, call } from 'redux-saga/effects';
+import { startWatchSearchActions } from './search/sagas';
 import { init } from './store/sagas';
 
 export default function* initialSaga() {
   yield all([
+    call(startWatchSearchActions),
     call(init),
   ]);
 }
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,7 @@
+// Application constants
+export const SEARCH_DEBOUNCE_TIME_IN_MS = 500;
+
+// API configs
 export const API_URL = 'https://self-service-app.herokuapp.com/';
 
 export const SEARCH_API_URL = '/products';
