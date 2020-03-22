30f2ce5e0c8e6420a4a99d0b45fe78a345dde9c5
Vladyslava Zhmurkevych <vladyslava_zhmurkevych@epam.com>
2020-03-22 14:41:19 +0200

    feat: add sign in page

---
+++ src/components/Input.js
@@ -0,0 +1,63 @@
+import {
+  StyleSheet, Text, TextInput, View,
+} from 'react-native';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { $gray } from '../constants/Colors';
+import Layout from '../constants/Layout';
+
+const { width } = Layout.window;
+
+export default function Input({
+  children, placeholder, keyboardType, label, secureTextEntry,
+}) {
+  return (
+    <>
+      {label && <Text style={styles.label}>{label}</Text>}
+      <View>
+        <TextInput
+          style={styles.field}
+          placeholder={placeholder}
+          placeholderTextColor={$gray}
+          keyboardType={keyboardType}
+          secureTextEntry={secureTextEntry}
+        />
+        {children}
+      </View>
+
+    </>
+
+  );
+}
+
+Input.defaultProps = {
+  children: null,
+  keyboardType: 'default',
+  label: null,
+  secureTextEntry: false,
+};
+
+Input.propTypes = {
+  children: PropTypes.node,
+  placeholder: PropTypes.string.isRequired,
+  keyboardType: PropTypes.string,
+  label: PropTypes.string,
+  secureTextEntry: PropTypes.bool,
+};
+
+const styles = StyleSheet.create({
+  field: {
+    width: width - 60,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: $gray,
+    fontSize: 20,
+    padding: 13,
+    paddingRight: 45,
+  },
+  label: {
+    fontSize: 20,
+    marginTop: 10,
+    marginBottom: 5,
+  },
+});
