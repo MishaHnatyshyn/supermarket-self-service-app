1c7dbf75bdaeb8417d492bbbab5606ed51415e4d
MishaHnatyshyn <gnatyshynmisha@gmail.com>
2020-03-29 23:09:21 +0300

    my account page

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -4,6 +4,9 @@ export const $gray = '#bdb9ba';
 export const $green = '#4cb922';
 export const $creamWhite = '#f9f9f7';
 export const $black = '#000';
+export const $red = 'rgba(255,86,79,0.74)';
+export const $semiDarkGray = 'gray';
+
 
 export default {
   tintColor: $tintColor,
